18e3e1500d7f5f00c612bde6937d268f77a7f81b
KoalaLoveTree <kiritokun2133@gmail.com>
2023-02-14 01:13:20 +0200

    Remove example comments ))0)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use phper::{functions::Argument, modules::Module, php_get_module, values::ZVal};
 use phper::sys::add_function;
 
-/// The php function, receive arguments with type `ZVal`.
 fn add_double(arguments: &mut [ZVal]) -> phper::Result<f64> {
     let x = arguments[0].expect_double()?;
     let y = arguments[1].expect_double()?;
@@ -9,7 +8,6 @@ fn add_double(arguments: &mut [ZVal]) -> phper::Result<f64> {
     Ok(x + y)
 }
 
-/// The php function, receive arguments with type `ZVal`.
 fn add_int(arguments: &mut [ZVal]) -> phper::Result<i64> {
     let x = arguments[0].expect_long()?;
     let y = arguments[1].expect_long()?;
@@ -17,8 +15,6 @@ fn add_int(arguments: &mut [ZVal]) -> phper::Result<i64> {
     Ok(x + y)
 }
 
-/// This is the entry of php extension, the attribute macro `php_get_module`
-/// will generate the `extern "C" fn`.
 #[php_get_module]
 pub fn get_module() -> Module {
     // New `Module` with extension info.
