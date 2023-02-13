70f403240cb5e6e0a9625f5bc40736d55cb82d27
KoalaLoveTree <kiritokun2133@gmail.com>
2023-02-14 01:12:52 +0200

    Remove example comments ))0)

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,8 @@
-// Copyright (c) 2022 PHPER Framework Team
-// PHPER is licensed under Mulan PSL v2.
-// You can use this software according to the terms and conditions of the Mulan
-// PSL v2. You may obtain a copy of Mulan PSL v2 at:
-//          http://license.coscl.org.cn/MulanPSL2
-// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
-// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-// See the Mulan PSL v2 for more details.
-
 use phper::{functions::Argument, modules::Module, php_get_module, values::ZVal};
 use phper::sys::add_function;
 
 /// The php function, receive arguments with type `ZVal`.
 fn add_double(arguments: &mut [ZVal]) -> phper::Result<f64> {
-    // Get the first argument, expect the type `ZStr`, and convert to Rust utf-8
-    // str.
     let x = arguments[0].expect_double()?;
     let y = arguments[1].expect_double()?;
 
@@ -23,8 +11,6 @@ fn add_double(arguments: &mut [ZVal]) -> phper::Result<f64> {
 
 /// The php function, receive arguments with type `ZVal`.
 fn add_int(arguments: &mut [ZVal]) -> phper::Result<i64> {
-    // Get the first argument, expect the type `ZStr`, and convert to Rust utf-8
-    // str.
     let x = arguments[0].expect_long()?;
     let y = arguments[1].expect_long()?;
 
@@ -42,7 +28,6 @@ pub fn get_module() -> Module {
         env!("CARGO_PKG_AUTHORS"),
     );
 
-    // Register function `add`, with one argument `name`.
     module
         .add_function("add_double", add_double)
         .argument(Argument::by_val("x"))
