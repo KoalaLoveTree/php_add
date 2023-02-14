db5fc13d7c81bd043909d3efd86a20d58841ce33
KoalaLoveTree <kiritokun2133@gmail.com>
2023-02-14 17:16:10 +0200

    Replace panic with error message returning

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use crate::ReturnType::{Double, Integer};
 use phper::sys::add_function;
-use phper::{functions::Argument, modules::Module, php_get_module, values::ZVal};
+use phper::{Error, functions::Argument, modules::Module, php_get_module, values::ZVal};
 
 enum ReturnType {
     Double,
@@ -8,45 +8,43 @@ enum ReturnType {
 }
 
 fn add(arguments: &mut [ZVal]) -> phper::Result<ZVal> {
-    let x = parse_numeric_arg(&mut arguments[0]);
-    let y = parse_numeric_arg(&mut arguments[1]);
+    let x = parse_numeric_arg(&mut arguments[0])?;
+    let y = parse_numeric_arg(&mut arguments[1])?;
 
-    let return_type = get_response_type_by_args_type(arguments);
+    let return_type = get_response_type_by_args_type(arguments)?;
 
-    let result = match return_type {
-        Double => ZVal::from(x + y),
-        Integer => ZVal::from((x + y) as i64),
-    };
-
-    Ok(result)
+    match return_type {
+        Double => Ok(ZVal::from(x + y)),
+        Integer => Ok(ZVal::from((x + y) as i64)),
+    }
 }
 
-fn parse_numeric_arg(argument: &mut ZVal) -> f64 {
+fn parse_numeric_arg(argument: &mut ZVal) -> Result<f64, Error> {
     match argument.expect_double() {
-        Ok(float) => float,
+        Ok(float) => Ok(float),
         Err(_err) => match argument.expect_long() {
-            Ok(int) => int as f64,
-            Err(err) => panic!("Input not a number: {err:?}"),
+            Ok(int) => Ok(int as f64),
+            Err(err) => Err(err),
         },
     }
 }
 
-fn get_response_type_by_args_type(arguments: &[ZVal]) -> ReturnType {
+fn get_response_type_by_args_type(arguments: &[ZVal]) -> Result<ReturnType, Error> {
     let return_type = match arguments[0].expect_double() {
         Ok(_float) => Double,
         Err(_err) => match arguments[0].expect_long() {
             Ok(_int) => Integer,
-            Err(err) => panic!("Input not a number: {err:?}"),
+            Err(err) => return Err(err),
         },
     };
 
     match return_type {
-        Double => Double,
+        Double => Ok(Double),
         Integer => match arguments[1].expect_double() {
-            Ok(_float) => Double,
+            Ok(_float) => Ok(Double),
             Err(_err) => match arguments[1].expect_long() {
-                Ok(_int) => Integer,
-                Err(err) => panic!("Input not a number: {err:?}"),
+                Ok(_int) => Ok(Integer),
+                Err(err) => Err(err),
             },
         },
     }
